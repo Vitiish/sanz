674132a77a83504096ad719985ab96ff6ae836e5
Vitiish <115109048+Vitiish@users.noreply.github.com>
2022-12-11 10:50:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("nav-menu").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("nav-menu").style.height = "0%";
+}
---
+++ topic.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("nav-menu").style.height = "100%";
+}
+  
+function closeNav() {
+  document.getElementById("nav-menu").style.height = "0%";
+}
+
